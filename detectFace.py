4f6114473cad006de5ecbd083cb91b79a0350c03
Will Xu <williamxu15@gmail.com>
2013-09-13 08:34:46 -0400

    updating detection

--- detectFace.py
+++ detectFace.py
@@ -1,17 +1,28 @@
 import cv2
+def main():
+	cascade = cv2.CascadeClassifier()
 
-cascade = cv2.CascadeClassifier()
+	cascade.load("haarcascades/haarcascade_frontalface_default.xml")
 
-cascade.load("haarcascades/haarcascade_frontalface_default.xml")
+	img = cv2.imread("face.jpg")
 
-img = cv2.imread("face.jpg")
+	answer = cascade.detectMultiScale(img)
 
-answer = cascade.detectMultiScale(img)
+	if len(answer) != 0:
+		for i in answer:
+			clearFace(img, (i[0], i[1]), (i[0]+i[2], i[1]+i[3]), (255,0,0))
 
-if len(answer) != 0:
-	for i in answer:
-		cv2.rectangle(img, (i[0], i[1]),(i[0]+i[2], i[1]+ i[3]), (255,0,0) )
+#if len(answer) != 0:
+#	for i in answer:
+#		cv2.rectangle(img, (i[0], i[1]),(i[0]+i[2], i[1]+ i[3]), (255,0,0) )
 
-cv2.imshow("",img)
-cv2.waitKey()
-cv2.destroyWindow("")
+	cv2.imshow("",img)
+	cv2.waitKey()
+	cv2.destroyWindow("")
+
+def clearFace():
+	pass
+
+
+
+if __name__ == "__main__": main()
