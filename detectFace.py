8e8ac195c79fea149ca689bc78f41ec52dde8557
Will Xu <williamxu15@gmail.com>
2013-09-25 09:08:22 -0400

    normalization occured

--- detectFace.py
+++ detectFace.py
@@ -25,12 +25,19 @@ def main():
 
 def clearFace(img, x, y, length, width, color = (255,0,0)):
 	arr = []
-	print "test"
 	for i in range(y, y+width):
 		for j in range(x, x+length):
 			arr.append( (img[j][i][0], img[j][i][1], img[j][i][2]) )
 	color = np.median(np.array(arr), axis = 0)
 	threshold = math.sqrt(color[0]**2 + color[1]**2 + color[2]**2)
+	print "threshold = %d" %threshold
+	notInThreshold = []
+	for i in range(y, y+width):
+		for j in range(x, x+length):
+			num = math.sqrt(img[j][i][0]**2+img[j][i][2]**2+img[j][i][1**2])
+			if (not num<threshold-20) or (not num>threshold+20):
+				notInThreshold.append(img[j][i])
+	print len(notInThreshold), x*y
 	#if colors are within threshold, go on
 	#if color is not within threshold, add to array and then continue
 	#after all locations are added into array, traverse through array and normalize colors
