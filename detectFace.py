ebafd5aa719431eaa4c9f06572e765e0f108fe69
Will Xu <williamxu15@gmail.com>
2013-12-04 08:26:06 -0500

    uptodate

--- detectFace.py
+++ detectFace.py
@@ -1,5 +1,4 @@
 import cv2
-import math
 import numpy as np
 
 def main():
@@ -7,37 +6,57 @@ def main():
 
 	cascade.load("haarcascades/haarcascade_frontalface_default.xml")
 
-	img = cv2.imread("face.jpg")
+	img = cv2.imread("photo.jpg")
 
 	answer = cascade.detectMultiScale(img)
 
+#	if len(answer) != 0:
+#		for i in answer:
+#			clearFace(img, i[0], i[1], i[2], i[3], (255,0,0))
 	if len(answer) != 0:
 		for i in answer:
-			clearFace(img, i[0], i[1], i[2], i[3], (255,0,0))
+			contours(img)
+#	if len(answer) != 0:
+#		for i in answer:
+#			cv2.rectangle(img, (i[0], i[1]),(i[0]+i[2], i[1]+ i[3]), (255,0,0) )
 
-#if len(answer) != 0:
-#	for i in answer:
-#		cv2.rectangle(img, (i[0], i[1]),(i[0]+i[2], i[1]+ i[3]), (255,0,0) )
+#		cv2.imshow("",img)
+#		cv2.waitKey()
+#		cv2.destroyWindow("")
 
-	#cv2.imshow("",img)
-	#cv2.waitKey()
-	#cv2.destroyWindow("")
+def contours(img):
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	redThresh = cv2.inRange(hsv, np.array((0,0,0)), np.array((10,100,100)))
+	erode = cv2.erode(redThresh, None, iterations = 3)
+	dilate = cv2.dilate(erode, None, iterations =10)
+	contours,hierarchy = cv2.findContours(redThresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-def clearFace(img, x, y, length, width, color = (255,0,0)):
+	for cnt in contours:
+		x,y,w,h = cv2.boundingRect(cnt)
+		cv2.rectangle(img, (x,y), ((x+w),(y+h)),[0,255,0],2)
+	cv2.imwrite("output.jpg", img)
+
+def clearFace(img, x, y, length, width, kolor = (255,0,0)):
+	N = 5
 	arr = []
+	ThreshNum = 150
 	for i in range(y, y+width):
 		for j in range(x, x+length):
 			arr.append( (img[j][i][0], img[j][i][1], img[j][i][2]) )
 	color = np.median(np.array(arr), axis = 0)
-	threshold = math.sqrt(color[0]**2 + color[1]**2 + color[2]**2)
-	print "threshold = %d" %threshold
+	rThresh = color[0]
+	gThresh = color[1]
+	bThresh = color[2]
+	print "threshold = %d %d %d" %(rThresh,gThresh,bThresh)
 	notInThreshold = []
 	for i in range(y, y+width):
 		for j in range(x, x+length):
-			num = math.sqrt(img[j][i][0]**2+img[j][i][2]**2+img[j][i][1**2])
-			if (not num<threshold-20) or (not num>threshold+20):
-				notInThreshold.append(img[j][i])
-	print len(notInThreshold), x*y
+			if abs(img[j][i][0]-rThresh)>ThreshNum or abs(img[j][i][1]-gThresh)>ThreshNum or abs(img[j][i][2]-bThresh)>ThreshNum:
+				notInThreshold.append((j,i))
+	print len(notInThreshold), width*length
+	for i in range(len(notInThreshold)):
+		cv2.circle(img, (notInThreshold[i][0], notInThreshold[i][1]), 1, (255,0,0))
+	cv2.imwrite("output.jpg", img)
 	#if colors are within threshold, go on
 	#if color is not within threshold, add to array and then continue
 	#after all locations are added into array, traverse through array and normalize colors
